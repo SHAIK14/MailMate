533bfe558257170caf50609741f0d2dcf599a113
shaik Asif <smd.20sa@gmail.com>
2023-03-23 00:48:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./components/Navbar";
-
+import Inbox from "./components/Inbox";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Inbox />
     </div>
   );
 }
